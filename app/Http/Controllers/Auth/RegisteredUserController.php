a3aa00e559b47ce3adb397f1fdf2cc300ea8565a
William Hatanaka(MiniDeskH470) <william.ariano@gmail.com>
2024-03-20 18:53:41 +0900

    add company name on register form, add creation of company, seller and user on user creation controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Models\Seller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
@@ -31,16 +34,39 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
+            'company_name' => ['required', 'string', 'max:255'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try {
+            DB::beginTransaction();
+
+            $company = Company::create([
+                'name' => $request->company_name,
+                'commission_rate' => 1,
+            ]);
+
+            $user = User::create([
+                'role_id' => 2,
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            Seller::create([
+                'company_id' => $company->id,
+                'user_id' => $user->id,
+            ]);
+
+            DB::commit();
+        }catch (\Exception $e){
+            dd($e);
+            DB::rollBack();
+            return redirect()->back()->with('error','Problem creating new user');
+        }
+
 
         event(new Registered($user));
 
